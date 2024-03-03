1a9bfa4611d4a3384bffd2bdbc18ad9e492bed8f
Kev <sulpeum@gmail.com>
2024-03-03 15:31:12 -0500

    feat: add generateToast func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { toast } from "sonner"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function generateToast({
+  type,
+  value,
+  description,
+}: {
+  type: "success" | "warning" | "error" | "info"
+  value?: string
+  description?: string
+}) {
+  switch (type) {
+    case "success":
+      return toast.success(value, { description })
+    case "warning":
+      return toast.warning(value, { description })
+    case "info":
+      return toast.info(value, { description })
+    case "error":
+      return toast.error(value, { description })
+    default:
+      return typeof type === "undefined" ? toast(value) : null
+  }
 }
