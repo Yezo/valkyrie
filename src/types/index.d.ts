515ebbea373a101c757edfd893d7c782d509f2be
Kev <sulpeum@gmail.com>
2024-03-04 15:58:11 -0500

    types: add types

--- src/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-// Reusable types
-// All types should end with only Type or Props (componentType or componentProps)
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+// Reusable types
+// All types should end with only Type or Props (componentType or componentProps)
+
+export type DatabasePromise =
+  | "success"
+  | "error"
+  | "user-duplicate"
+  | "email-duplicate"
+  | "not-found"
+  | "invalid-input"
+  | null
